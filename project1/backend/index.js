1e049c9ea68a43cec3b812f50d54d9230ff0bdfa
ramjigupta2005 <ramjigupta2005@gmail.com>
2025-05-13 23:21:32 +0530

    Refactor mytask.html by removing unused CSS classes and comments

--- project1/backend/index.js
+++ project1/backend/index.js
@@ -9,8 +9,8 @@ const { mongoUrl } = require('./config')
 const dotenv = require('dotenv')
 require('dotenv').config()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use('/user', userRouter)
 app.use('/todo', todoRouter)
 mongoose.connect(mongoUrl).then(()=>{
